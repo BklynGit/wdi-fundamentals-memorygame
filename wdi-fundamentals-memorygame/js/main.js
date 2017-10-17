ae257a12f79a80aa393585c2c6bec49de24b4842
BklynGit <32805579+BklynGit@users.noreply.github.com>
2017-10-16 22:30:19 -0400

    created game logic

---
+++ wdi-fundamentals-memorygame/js/main.js
@@ -0,0 +1,9 @@
+
+
+var cardOne = "queen";
+var cardTwo = "queenOfHearts";
+var cardThree = "king";
+var cardFour = "kingOfHearts";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
