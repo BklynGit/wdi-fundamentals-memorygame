abf6ac1a19f083c80a91135a0234cb602b8f8540
BklynGit <32805579+BklynGit@users.noreply.github.com>
2017-10-17 16:53:41 -0400

    created game logic

--- wdi-fundamentals-memorygame/js/main.js
+++ wdi-fundamentals-memorygame/js/main.js
@@ -1,9 +1,20 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+	cardsInPlay.push("queen"); 
+console.log("User flipped queen");
+
+var cardTwo = cards[1];
+	cardsInPlay.push("king");
+console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
 
 
-var cardOne = "queen";
-var cardTwo = "queenOfHearts";
-var cardThree = "king";
-var cardFour = "kingOfHearts";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
