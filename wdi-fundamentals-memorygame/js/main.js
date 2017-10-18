19e83ca627fedbd6767b201c9e8fa8cf072d3352
BklynGit <32805579+BklynGit@users.noreply.github.com>
2017-10-18 13:50:27 -0400

    created game logic

--- wdi-fundamentals-memorygame/js/main.js
+++ wdi-fundamentals-memorygame/js/main.js
@@ -1,20 +1,52 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-	cardsInPlay.push("queen"); 
-console.log("User flipped queen");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+	}
+}
 
-var cardTwo = cards[1];
-	cardsInPlay.push("king");
-console.log("User flipped king");
+
+var flipCard = function (cardID) {
+	cardsInPlay.push(cards[cardID].rank);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+
 
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
     alert("Sorry, try again.");
+    
+	}
 }
 
 
-
-
+flipCard(0);
+flipCard(2);
